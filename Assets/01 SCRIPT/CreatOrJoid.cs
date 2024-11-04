cadddb53ef369171db53fbdf3c24d872ab3c2568
phammquan <phamminhq91@gmail.com>
2024-11-04 19:28:28 +0700

    test goku hai may

--- Assets/01 SCRIPT/CreatOrJoid.cs
+++ Assets/01 SCRIPT/CreatOrJoid.cs
@@ -6,20 +6,20 @@ using Photon.Pun;
 
 public class CreatOrJoid : MonoBehaviourPunCallbacks
 {
-    public InputField _createInputField;
-    public InputField _joidInputField;
+  public InputField _createInputField;
+  public InputField _joidInputField;
 
 
-    public void CreatRoom()
-    {
-        PhotonNetwork.CreateRoom(_createInputField.text);
-    }
-    public void JoidRoom()
-    {
-        PhotonNetwork.JoinRoom(_joidInputField.text);
-    }
-    public override void OnJoinedRoom()
-    {
-        PhotonNetwork.LoadLevel("GamePlay");
-    }
+  public void CreatRoom()
+  {
+    PhotonNetwork.CreateRoom(_createInputField.text);
+  }
+  public void JoidRoom()
+  {
+    PhotonNetwork.JoinRoom(_joidInputField.text);
+  }
+  public override void OnJoinedRoom()
+  {
+    PhotonNetwork.LoadLevel("GamePlay");
+  }
 }
